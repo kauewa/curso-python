723368463b9a3bc11034cae58cdd056bf35f4414
kauewa <wandscherkaue@gmail.com>
2022-06-09 16:22:20 -0300

    aprendendo os comandos break e continue

--- advinhacao.py
+++ advinhacao.py
@@ -11,9 +11,14 @@ for rodada in range(1, total_de_tentativas + 1):
 
     print("Tentativas {} de {}".format(rodada, total_de_tentativas))
     
-    #Capturando valor do usuário
-    numero = input("Digite um número: ")
+    #Capturando e verificando valor do usuário
+    numero = input("Digite um número entre 1 e 100: ")
     chute = int(numero)
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+
 
     #variáveis de resultado
     acertou = chute == numero_chave
@@ -23,6 +28,7 @@ for rodada in range(1, total_de_tentativas + 1):
     #Checagem do acerto
     if(acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):
             print("seu chute foi maior que o número secreto!")

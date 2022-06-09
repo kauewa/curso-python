5c72ea5ac6fe53b3a7acd8d06a5e03b50c9f9221
kauewa <wandscherkaue@gmail.com>
2022-06-09 15:30:44 -0300

    segundo commit(aula)

--- advinhacao.py
+++ advinhacao.py
@@ -1,14 +1,26 @@
+#Inicio
 print ("jogo de advinhação!")
-
 numero = input("Digite um número: ")
-chave = int(numero)
-print ("seu número é: ", chave)
+
+print ("seu número é: ", numero)
+
+#Variáveis
+chute = int(numero)
 numero_chave = 42
 
+acertou = chute == numero_chave
+maior   = chute > numero_chave
+menor   = chute < numero_chave
 
-if(numero_chave == numero):
+#Processamento
+if(acertou):
     print("Você acertou!")
 else:
+    if(maior):
+        print("seu chute foi maior que o número secreto!")
+    if(menor):
+        print("seu chute foi menor que o número secreto!")
+
     print("Errrooooouuuuuu!")
 
 print("fim de jogo")

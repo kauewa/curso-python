d949fba9c612ae28719b89e7d008ab0f767c5bd9
kauewa <wandscherkaue@gmail.com>
2022-06-09 15:56:35 -0300

    Terceiro commit(aula)

--- advinhacao.py
+++ advinhacao.py
@@ -1,26 +1,35 @@
 #Inicio
 print ("jogo de advinhação!")
-numero = input("Digite um número: ")
 
-print ("seu número é: ", numero)
-
-#Variáveis
-chute = int(numero)
+#Variáveis iniciais
 numero_chave = 42
-
-acertou = chute == numero_chave
-maior   = chute > numero_chave
-menor   = chute < numero_chave
+total_de_tentativas = 3
+rodada = 1
 
 #Processamento
-if(acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("seu chute foi maior que o número secreto!")
-    if(menor):
-        print("seu chute foi menor que o número secreto!")
+while(rodada <= total_de_tentativas):
 
-    print("Errrooooouuuuuu!")
+    print("Tentativas", rodada, "de 3")
+    
+    #Capturando valor do usuário
+    numero = input("Digite um número: ")
+    chute = int(numero)
+
+    #variáveis de resultado
+    acertou = chute == numero_chave
+    maior   = chute > numero_chave
+    menor   = chute < numero_chave  
+    
+    #Checagem do acerto
+    if(acertou):
+        print("Você acertou!")
+        total_de_tentativas = 0
+    else:
+        if(maior):
+            print("seu chute foi maior que o número secreto!")
+        if(menor):
+            print("seu chute foi menor que o número secreto!")
+        print("Errrooooouuuuuu!")
+        rodada = rodada + 1
 
 print("fim de jogo")

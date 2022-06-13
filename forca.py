9b57212208970acc2d4cc83dfe23555f92315fb7
kauewa <wandscherkaue@gmail.com>
2022-06-13 09:37:18 -0300

    começando jogo da forca

--- forca.py
+++ forca.py
@@ -1,6 +1,27 @@
+from operator import index
+
+
 def jogo():
     print("jogo da forca")
 
+    palavar_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra?")
+
+        index = 0
+        for letra in palavar_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("jogando ...")
+
+    print("Fim de jogo!")
 
 
 

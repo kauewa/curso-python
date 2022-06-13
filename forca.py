719804b2978f73aab3f38cf64a74e09af0cd3456
kauewa <wandscherkaue@gmail.com>
2022-06-13 11:24:05 -0300

    at

--- forca.py
+++ forca.py
@@ -1,23 +1,53 @@
 from operator import index
+import random
 
 
 def jogo():
     print("jogo da forca")
 
-    palavar_secreta = "banana"
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavar_secreta = palavras[numero].upper()
+    letras_acertadas = ["_" for letra in palavar_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
 
     while(not enforcou and not acertou):
         chute = input("Qual letra?")
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavar_secreta:
-            if(chute == letra):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
+        if(chute in palavar_secreta):
+            index = 0
+            for letra in palavar_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+
+    if(acertou):
+        print("Você acertou")
+    else:
+        print("você perdeu")
+    
+
+
+        print(letras_acertadas)
 
         print("jogando ...")
 
